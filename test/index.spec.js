edd5d93988217ae79b714b6b3be2db54848b9fd2
Aravindan Ve <aravindan.ve@outlook.com>
2018-11-27 00:16:56 +0530

    add json-schema-test and test suite, fix bugs in compile and validate

---
+++ test/index.spec.js
@@ -0,0 +1,62 @@
+const chai = require('chai');
+const jst = require('json-schema-test');
+const josie = require('..');
+
+function afterEach(result) {
+  // result is an object with properties:
+  //   validator, schema, data,
+  //   valid (validation result), expected (expected validation result),
+  //   errors (array of errors or null), passed (true if valid == expected)
+
+  // you can do some additional validation and logging
+  // ...
+  // console.log(res);
+
+  // Pass option log == false to prevent default error logging
+
+  // If result.passed is false the test will fails after this function returns
+}
+
+function afterError(err) {
+  // result is an object with properties:
+  //   validator, schema, data,
+  //   valid (validation result), expected (expected validation result),
+  //   errors (array of errors or null), passed (true if valid == expected)
+
+  // you can do some additional validation and logging
+  // ...
+  // console.log(res);
+
+  // Pass option log == false to prevent default error logging
+
+  // If result.passed is false the test will fails after this function returns
+}
+
+jst({
+  validate(schema, data) {
+    try {
+      josie.compile(schema).validate(data);
+      this.errors = [];
+      return true;
+
+    } catch (err) {
+      this.errors = [err];
+      return false;
+    }
+  },
+  errors: []
+
+}, {
+  description: 'Josie Schema Tests',
+  suites: {
+    'JSON-Schema-Test-Suite/draft-07': './JSON-Schema-Test-Suite/tests/draft7/{**/,}*.json',
+    // 'Advanced Tests': './tests/{**/,}*.json'
+  },
+  async: false,
+  afterEach,
+  afterError,
+  log: true,
+  cwd: __dirname,
+  timeout: 10000,
+  assert: chai.assert
+});
